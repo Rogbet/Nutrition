9e954eff149c869d5836856873a69cf078f2d464
Roger <roge.villafu@gmail.com>
2016-05-03 23:10:16 -0500

    Downgrade AspNet MVC 5

---
+++ Nutrition/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nutrition.MvcApplication" Language="C#" %>
---
+++ Nutrition/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nutrition.Startup))]
+namespace Nutrition
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
