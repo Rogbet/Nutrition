2aff46f56a2dcc0256d6ee6b7770d5b18c375f48
rogbet <rogbet@hotmail.com>
2016-05-20 19:21:53 -0500

    metro style added

--- Nutrition/Scripts/app/route-config.js
+++ Nutrition/Scripts/app/route-config.js
@@ -14,14 +14,30 @@ angular.module('app')
     .config(function ($stateProvider, $urlRouterProvider) {
         //
         // For any unmatched url, redirect to /state1
-        $urlRouterProvider.otherwise("/");
+        $urlRouterProvider.otherwise("/home");
         //
         // Now set up the states
         $stateProvider
-          .state('patients', {
-              url: "/patients",
-              templateUrl: "/scripts/app/patient/patientlist.html",
-              controller: 'PatientController',
-              controllerAs: 'vm'
-          })
+            .state('home', {
+                url: "/home",
+                templateUrl: "/scripts/app/home/index.html"
+            })
+        .state('patientcreate', {
+            url: "/patient/create",
+            templateUrl: "/scripts/app/patient/create.html",
+            controller: 'PatientController',
+            controllerAs: 'vm'
+        })
+        .state('patient', {
+            url: "/patient/{id}",
+            templateUrl: "/scripts/app/patient/index.html",
+            controller: 'PatientController',
+            controllerAs: 'vm'
+        })
+        .state('patientlist', {
+            url: "/patients",
+            templateUrl: "/scripts/app/patient/list.html",
+            controller: 'PatientController',
+            controllerAs: 'vm'
+        })
     });
