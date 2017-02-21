ed43a94096d717c00e400b0f03fb0b636db35604
Roger <roge.villafu@gmail.com>
2017-02-21 01:53:26 -0600

    initial commit login

---
+++ app/src/app.module.js
@@ -0,0 +1,37 @@
+(function() {
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('app', [
+  'ngRoute',
+  'app.version',
+  // Third party modules.
+  'firebase',
+
+  // Custom modules.
+  'app.auth',
+  'app.core',
+  'app.landing',
+  'app.layout',
+  //'app.waitList'
+])
+.config(configFunction)
+.run(runFunction);
+
+function configFunction($routeProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+  }
+
+  runFunction.$inject = ['$rootScope', '$location'];
+
+  function runFunction($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+      if (error === "AUTH_REQUIRED") {
+        $location.path('/');
+      }
+    });
+  }
+
+})();
